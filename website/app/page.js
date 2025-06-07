0c6cf4e3fb40e3479776e8916e0e70d626b51545
MinhLam577 <duynghia0705@gmail.com>
2025-06-08 00:00:08 +0700

    fix login ui

--- website/app/page.js
+++ website/app/page.js
@@ -2,14 +2,14 @@ import Home from "./(home)/home";
 
 import Wrapper from "./layout/wrapper";
 export const metadata = {
-  title: "Ô Tô Hồng Sơn || Chuyên mua bán và trao đổi xe ô tô uy tín",
-  description: `Ô Tô Hồng Sơn cung cấp đa dạng dòng xe với giá tốt nhất, hỗ trợ tài chính linh hoạt và được tin cậy bởi hàng ngàn khách hàng.`,
+    title: "Ô Tô Hồng Sơn",
+    description: `Ô Tô Hồng Sơn cung cấp đa dạng dòng xe với giá tốt nhất, hỗ trợ tài chính linh hoạt và được tin cậy bởi hàng ngàn khách hàng.`,
 };
 
 export default function MainRoot() {
-  return (
-    <Wrapper>
-      <Home />
-    </Wrapper>
-  );
+    return (
+        <Wrapper>
+            <Home />
+        </Wrapper>
+    );
 }
