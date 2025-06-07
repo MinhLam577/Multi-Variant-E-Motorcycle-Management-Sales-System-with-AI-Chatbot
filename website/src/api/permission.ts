1e28f86b4d8c1682a68431daae39a8c0e2c1fb6f
MinhLam577 <duynghia0705@gmail.com>
2025-06-08 00:00:08 +0700

    fix login ui

--- website/src/api/permission.ts
+++ website/src/api/permission.ts
@@ -1,18 +1,44 @@
-import { IPermission } from "src/types/backend";
 import apiClient from "./apiClient";
 import endpoints from "./endpoints";
+export interface IPermission {
+    id?: string;
+    name?: string;
+    // path
+    path?: string;
+    method?: string;
+    module?: string;
+
+    createdBy?: string;
+    isDeleted?: boolean;
+    deletedAt?: boolean | null;
+    createdAt?: string;
+    updatedAt?: string;
+}
+
+export interface IRole {
+    id?: string;
+    name: string;
+    description: string;
+    permissions: IPermission[] | string[];
+    isActive: boolean;
+    createdBy?: string;
+    isDeleted?: boolean;
+    deletedAt?: boolean | null;
+    createdAt?: string;
+    updatedAt?: string;
+}
 
 export interface PermissionResponse {
-  data: IPermission[];
-  status: number;
-  message: string;
+    data: IPermission[];
+    status: number;
+    message: string;
 }
 const Permissons = {
-  getListPermissions: async (): Promise<PermissionResponse> => {
-    const Response = await apiClient.get<any, PermissionResponse>(
-      endpoints.permission.list(",", "")
-    );
-    return Response;
-  },
+    getListPermissions: async (): Promise<PermissionResponse> => {
+        const Response = await apiClient.get<any, PermissionResponse>(
+            endpoints.permission.list(",", "")
+        );
+        return Response;
+    },
 };
 export default Permissons;
--- website/src/stores/brand.js
+++ website/src/stores/brand.js
@@ -4,35 +4,34 @@ import endpoints from "../api/endpoints.ts";
 import { RootStore } from "./base";
 
 class BrandObservable {
-  errorMsg = "";
-  status = "initial";
-  listBrand = [];
-  rootStore = RootStore;
-  constructor(rootStore) {
-    makeAutoObservable(this);
-    this.rootStore = rootStore;
-  }
-
-  *getListBrand({ page, size }) {
-    this.status = "submitting";
-    try {
-      const { data, status } = yield apiClient.get(
-        endpoints.brand.list(page, size)
-      );
-
-      if (status !== 200 && !data) {
-        this.status = "loginFailed";
-        this.errorMsg = "Email hoặc mật khẩu không đúng!";
-        return;
-      }
-      console.log(data);
-      this.listBrand = data?.result;
-      this.status = "fetchSuccess";
-    } catch (error) {
-      this.status = "fetchFailed";
-      this.errorMsg = error?.message;
+    errorMsg = "";
+    status = "initial";
+    listBrand = [];
+    rootStore = RootStore;
+    constructor(rootStore) {
+        makeAutoObservable(this);
+        this.rootStore = rootStore;
     }
-  }
+
+    *getListBrand({ page, size }) {
+        this.status = "submitting";
+        try {
+            const { data, status } = yield apiClient.get(
+                endpoints.brand.list(page, size)
+            );
+
+            if (status !== 200 && !data) {
+                this.status = "loginFailed";
+                this.errorMsg = "Email hoặc mật khẩu không đúng!";
+                return;
+            }
+            this.listBrand = data?.result;
+            this.status = "fetchSuccess";
+        } catch (error) {
+            this.status = "fetchFailed";
+            this.errorMsg = error?.message;
+        }
+    }
 }
 
 export default BrandObservable;
