49abfb82dde1a251e6b492d5cf3fcf278e36177e
Ngô Đình Phước <ngodinhphuoc100@gmail.com>
2025-06-12 00:59:05 +0700

    add page success for login google

--- website/app/components/pages/login/Form.js
+++ website/app/components/pages/login/Form.js
@@ -8,115 +8,113 @@ import ModalChangePassword from "../../modal/modal.changePassword";
 
 import { useRouter } from "next/navigation";
 const Form = observer(() => {
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const [userEmail, setUserEmail] = useState("");
-    const [changePassword, setChangePassword] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [userEmail, setUserEmail] = useState("");
+  const [changePassword, setChangePassword] = useState(false);
 
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [remember, setRemember] = useState(false);
-    const { loginObservable } = useStore();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [remember, setRemember] = useState(false);
+  const { loginObservable } = useStore();
 
-    const router = useRouter();
-    const validateEmail = (email) => {
-        // Regex cơ bản để kiểm tra định dạng email
-        const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        return re.test(email);
-    };
+  const router = useRouter();
+  const validateEmail = (email) => {
+    // Regex cơ bản để kiểm tra định dạng email
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return re.test(email);
+  };
 
-    const onSubmit = async (e) => {
-        e.preventDefault();
-        setUserEmail("");
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    setUserEmail("");
 
-        if (!email || !password) {
-            message.error("Vui lòng nhập đầy đủ thông tin.");
-            return;
-        }
+    if (!email || !password) {
+      message.error("Vui lòng nhập đầy đủ thông tin.");
+      return;
+    }
 
-        if (!validateEmail(email)) {
-            message.error("Email không hợp lệ.");
-            return;
-        }
+    if (!validateEmail(email)) {
+      message.error("Email không hợp lệ.");
+      return;
+    }
 
-        if (password.length < 6) {
-            message.error("Mật khẩu phải có ít nhất 6 ký tự.");
-            return;
-        }
-        await loginObservable.login(email, password);
+    if (password.length < 6) {
+      message.error("Mật khẩu phải có ít nhất 6 ký tự.");
+      return;
+    }
+    await loginObservable.login(email, password);
 
-        if (loginObservable.status == "loginSuccess") {
-            message.success(loginObservable.successMsg);
-            router.push("/");
-        } else {
-            message.error(loginObservable.errorMsg);
-            // cần kích hoạt
-            if (loginObservable.code == 2) {
-                setIsModalOpen(true);
-                setUserEmail(email);
-                return;
-            }
-        }
-    };
-    return (
-        <form onSubmit={onSubmit}>
-            <div className="mb-2 mr-sm-2">
-                <label className="form-label">
-                    Username or email address *
-                </label>
-                <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    required
-                />
-            </div>
-            <div className="form-group mb5">
-                <label className="form-label">Password *</label>
-                <input
-                    type="password"
-                    className="form-control"
-                    placeholder="Password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    required
-                />
-            </div>
-            <div className="custom-control custom-checkbox">
-                <input
-                    type="checkbox"
-                    className="custom-control-input"
-                    id="exampleCheck3"
-                    checked={remember}
-                    onChange={(e) => setRemember(e.target.checked)}
-                />
-                <label className="custom-control-label" htmlFor="exampleCheck3">
-                    Remember me
-                </label>
-                <a
-                    className="btn-fpswd float-end"
-                    href="#"
-                    onClick={() => setChangePassword(true)}
-                >
-                    Lost your password?
-                </a>
-            </div>
-            <button type="submit" className="btn btn-log btn-thm mt5">
-                Sign in
-            </button>
-            <ModalReactive
-                isModalOpen={isModalOpen}
-                setIsModalOpen={setIsModalOpen}
-                userEmail={userEmail}
-            />
-            <ModalChangePassword
-                isModalOpen={changePassword}
-                setIsModalOpen={setChangePassword}
-            />
-            ;
-        </form>
-    );
+    if (loginObservable.status == "loginSuccess") {
+      message.success(loginObservable.successMsg);
+      router.push("/");
+    } else {
+      message.error(loginObservable.errorMsg);
+      // cần kích hoạt
+      if (loginObservable.code == 2) {
+        setIsModalOpen(true);
+        setUserEmail(email);
+        return;
+      }
+    }
+  };
+  return (
+    <form onSubmit={onSubmit}>
+      <div className="mb-2 mr-sm-2">
+        <label className="form-label">Username or email address *</label>
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+      </div>
+      <div className="form-group mb5">
+        <label className="form-label">Password *</label>
+        <input
+          type="password"
+          className="form-control"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+      </div>
+      <div className="custom-control custom-checkbox">
+        <input
+          type="checkbox"
+          className="custom-control-input"
+          id="exampleCheck3"
+          checked={remember}
+          onChange={(e) => setRemember(e.target.checked)}
+        />
+        <label className="custom-control-label" htmlFor="exampleCheck3">
+          Remember me
+        </label>
+        <a
+          className="btn-fpswd float-end"
+          href="#"
+          onClick={() => setChangePassword(true)}
+        >
+          Lost your password?
+        </a>
+      </div>
+      <button type="submit" className="btn btn-log btn-thm mt5">
+        Sign in
+      </button>
+      <ModalReactive
+        isModalOpen={isModalOpen}
+        setIsModalOpen={setIsModalOpen}
+        userEmail={userEmail}
+      />
+      <ModalChangePassword
+        isModalOpen={changePassword}
+        setIsModalOpen={setChangePassword}
+      />
+      ;
+    </form>
+  );
 });
 
 export default Form;
--- website/src/stores/login.js
+++ website/src/stores/login.js
@@ -3,72 +3,100 @@ import apiClient from "../api/apiClient";
 import endpoints from "../api/endpoints.ts";
 import { RootStore } from "./base";
 import { handleErrorMessage } from "../api/handleErrorMessage";
+import axios from "axios";
 
 export class LoginObservable {
-    errorMsg = "";
-    successMsg = "";
-    status = "initial";
-    code = "";
-    rootStore = RootStore;
-    constructor(rootStore) {
-        makeAutoObservable(this);
-        this.rootStore = rootStore;
-    }
-    // call api login
-    *login(email, password) {
-        this.status = "submitting";
-        try {
-            const { data, status, message } = yield apiClient.post(
-                endpoints.auth.login,
-                {
-                    email,
-                    password,
-                }
-            );
-
-            if (status !== 200 && !data?.userId) {
-                this.status = "loginFailed";
-                this.errorMsg = message;
-                if (
-                    message ==
-                    "Tài khoản chưa được kích hoạt. Vui lòng kiểm tra email để kích hoạt tài khoản nha"
-                )
-                    this.code = 2;
-                return;
-            }
-            yield this.rootStore.accountObservable.setAccount(data);
-            yield this.rootStore.userObservable.getMe(data.userId);
-            this.status = "loginSuccess";
-            this.successMsg = message;
-        } catch (error) {
-            this.status = "loginFailed";
-            this.errorMsg = error?.message;
+  errorMsg = "";
+  successMsg = "";
+  status = "initial";
+  code = "";
+  rootStore = RootStore;
+  constructor(rootStore) {
+    makeAutoObservable(this);
+    this.rootStore = rootStore;
+  }
+  // call api login
+  *login(email, password) {
+    this.status = "submitting";
+    try {
+      const { data, status, message } = yield apiClient.post(
+        endpoints.auth.login,
+        {
+          email,
+          password,
         }
-    }
-    //forgot password
-    *forgotPassword(email) {
-        try {
-            const { data, status } = yield apiClient.post(
-                endpoints.auth.forgotPassword,
-                {
-                    email,
-                }
-            );
-            if (status !== 200) {
-                this.status = "forgotPasswordFailed";
-                this.errorMsg = "Email không tồn tại!";
-                return;
-            }
-            this.status = "forgotPasswordSuccess";
-        } catch (error) {
-            this.status = "forgotPasswordFailed";
-            this.errorMsg = handleErrorMessage(error);
-        }
-    }
+      );
 
-    *logout() {
-        yield this.rootStore.accountObservable.clearAccount();
+      if (status !== 200 && !data?.userId) {
+        this.status = "loginFailed";
+        this.errorMsg = message;
+        if (
+          message ==
+          "Tài khoản chưa được kích hoạt. Vui lòng kiểm tra email để kích hoạt tài khoản nha"
+        )
+          this.code = 2;
+        return;
+      }
+      yield this.rootStore.accountObservable.setAccount(data);
+      yield this.rootStore.userObservable.getMe(data.userId);
+      this.status = "loginSuccess";
+      this.successMsg = message;
+    } catch (error) {
+      this.status = "loginFailed";
+      this.errorMsg = error?.message;
     }
+  }
+  //forgot password
+  *forgotPassword(email) {
+    try {
+      const { data, status } = yield apiClient.post(
+        endpoints.auth.forgotPassword,
+        {
+          email,
+        }
+      );
+      if (status !== 200) {
+        this.status = "forgotPasswordFailed";
+        this.errorMsg = "Email không tồn tại!";
+        return;
+      }
+      this.status = "forgotPasswordSuccess";
+    } catch (error) {
+      this.status = "forgotPasswordFailed";
+      this.errorMsg = handleErrorMessage(error);
+    }
+  }
+
+  catch(error) {
+    this.status = "loginFailed";
+    this.errorMsg = handleErrorMessage(error);
+  }
+  // call api login
+  *getProfile_ByGoogle(token) {
+    this.status = "submitting";
+    try {
+      const { data } = yield axios.get(
+        process.env.NEXT_PUBLIC_BACK_END_API_BASE_URL +
+          endpoints.customers.loginGoogle,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      yield this.rootStore.accountObservable.setAccount(data.data);
+      // yield this.rootStore.userObservable.getMe(data.userId);
+      this.status = "loginSuccess";
+      this.successMsg = "Đăng nhập thành công";
+    } catch (error) {
+      this.status = "loginFailed";
+      this.errorMsg = error?.message;
+    }
+  }
+
+  *logout() {
+    yield this.rootStore.accountObservable.clearAccount();
+  }
 }
 
 export default LoginObservable;
